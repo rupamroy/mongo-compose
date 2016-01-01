1c7c5591b01ce3043ef3b093ab5baa41b3035269
rupamroy <roopamroy@gmail.com>
2015-12-31 20:33:22 -0500

    Simple Node start

---
+++ app.js
@@ -0,0 +1,5 @@
+var data = require("./data");
+
+data.data.forEach(function(e){
+   console.log(e.name);
+});
---
+++ data.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getData: function () {
+        return [
+            {_id: "1ee7270c-7bc7-4533-ac49-e61f05a364d6", name: "Rupam Roy"},
+            {_id: "a5bf2e1d-c959-4c1a-9ead-bfe757e65145", name: "Jack Aster"}
+
+        ];
+    }
+};
